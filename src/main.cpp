eaebf71777200030a11a0804e78b94f030ab54da
handymathis@gmail..com <ot&u$SLh8q%@#5qW6js@5&BSRTpwNGCWmH*pTG!Eh5xyKewYndLT^!BWHPEebcYuj4JNkbHz>
2021-06-15 07:51:19 +0200

    sending all potentiometer states on startup

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,29 @@
 
 Protocols prots = Protocols(176, 50);
 
+// Arduino channel for potientiometer
 uint8_t channel[] = {A0};
+
+// Numbers of the MIDI channels (same indexes as the channel[])
 int channelNumbers[] = {21};
+
 int channelValues[(int)sizeof(channel)];
 
-
+// This function reads all potentiometer states once
+// and sends them
 void setup() {
   Serial.begin(9600);
 
   for (int i=0; i<(int)sizeof(channel); i++) {
-    channelValues[i] = analogRead(channel[i]);
+    int value = analogRead(channel[i]);
+    channelValues[i] = value;
+    prots.send_MIDI_cmd(channelNumbers[i], prots.ConvertPotValue(value));
   }
 }
 
+// This loop runs infinitely and 
+// executes the listening actions on
+// the arduino
 void loop() {
 
     checkFader(prots, channel, channelValues, channelNumbers);
