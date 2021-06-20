fe71f4d8699888924a11ece58da3808fb9e21189
Mathis <handymathis@gmail.com>
2021-06-20 16:14:05 +0200

    fixed

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <Arduino.h>
-#include "./protocols/protocols.h"
+#include "protocols/protocols.h"
 
-Protocols prots = Protocols(176, 50);
+#define MIDI_CHANNEL 176
+#define TOLLERANCE 50
+
+Protocols prots = Protocols(MIDI_CHANNEL, TOLLERANCE);
 
 uint8_t channel[] = {A0};
 int controller[] = {21};
@@ -17,13 +20,14 @@ void setup() {
 
 void loop() {
 
+    // This loop checks, if any values of the
+    // potentiometer changed
     for (int i=0; i<(int)sizeof(channel); i++) {
-      int value = analogRead(channel[i]);
-      if (prots.tolleranceTriggered(channelValues[i], value)) {
-        channelValues[i] = value;
-        prots.send_MIDI_cmd(controller[i], value);
-      }
-    }
-    delay(1000);
 
+        int value = analogRead(channel[i]);
+        if (prots.tolleranceTriggered(channelValues[i], value)) {
+            channelValues[i] = value;
+            prots.send_MIDI_cmd(controller[i], value);
+        }
+    }
 }
