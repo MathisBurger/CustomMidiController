f1ffe317c919820da9534854614b14fbdc479c32
Mathis <handymathis@gmail.com>
2021-07-26 22:58:37 +0200

    fixed hex file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 #include "protocols/protocols.h"
-#include "checker/checker.h"
 
 #define MIDI_CHANNEL 176
 #define TOLLERANCE 200
@@ -8,8 +7,8 @@
 Protocols prots = Protocols(MIDI_CHANNEL, TOLLERANCE);
 
 // Arduino channel for potientiometer
-uint8_t channel[] = {A0};
-int controller[] = {21};
+uint8_t channel[] = {A1, A2, A3, A4, A5, A6};
+int controller[] = {21, 22, 23, 24, 25, 26};
 
 // Numbers of the MIDI channels (same indexes as the channel[])
 int channelNumbers[] = {21};
@@ -22,7 +21,7 @@ int channelValues[(int)sizeof(channel)];
 void setup() {
   Serial.begin(9600);
 
-  for (int i=0; i<(int)sizeof(channel); i++) {
+  for (int i=0; i<sizeof(channel); i++) {
     int value = analogRead(channel[i]);
     channelValues[i] = value;
     prots.send_MIDI_cmd(channelNumbers[i], prots.ConvertPotValue(value));
